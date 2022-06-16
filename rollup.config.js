cab9bc9fe9e98a3d7dfa211239cf577600a1a052
rusin <rusin.senya@mail.ru>
2022-06-16 09:20:00 +0300

    Fix feedback

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-    input: 'Scripts/RandomFact.ts',
-    output: {
-        file: 'bundle.js',
-    },
+    input: 'Scripts/RandomFact.js',
+    output: { file: 'bundle.js' },
+    plugins : [
+        resolve({ browser: true }),
+        terser(),
+    ]
 };
