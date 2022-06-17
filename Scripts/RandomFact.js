56e086ccd29d818a6c31d5ca8e5cf9baeca1fac5
rusin <rusin.senya@mail.ru>
2022-06-17 14:21:56 +0300

    test if new API works

--- Scripts/RandomFact.js
+++ Scripts/RandomFact.js
@@ -1,32 +1,29 @@
 
 // assign variables their interfaces for type check
-var button = document.getElementById("submitButton") as HTMLButtonElement
-var userName = document.getElementById("name") as HTMLFormElement
-
+var button = document.getElementById("submitButton")
+var userName = document.getElementById("name") 
 async function fetchType(name) {
     try {
         console.log(name)
         const response = await fetch(`https://api.agify.io?name=${name.toLowerCase()}`)
         const data = await response.json()
-        console.log(data)
-        //printFact(data)
+        printFact(data.age)
     } catch (e) {
         console.log("There was a problem fetching number.")
     }
 }
-
 button.onclick = function () {
     fetchType(userName.value)
 }
 
-function printFact(fact) {
+function printFact(age) {
     var factEl = document.getElementById("fact");
     // check if this element is null
     if (factEl == null) throw new Error('Cannot find the fact element');
     // else we can safely assign value without type checking
-    else document.getElementById("fact")!.innerHTML = fact;
+    else document.getElementById("fact").innerHTML = "Your age - " + age;
     // assign interface
-    var textField = document.getElementById("fact") as HTMLElement
+    var textField = document.getElementById("fact")
     textField.style.visibility = "visible"
 }
 
